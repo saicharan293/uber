1aeedcc581970b6a2da7e4eec1d7a94aa2a13820
saicharan293 <2493saicharan@gmail.com>
2024-12-06 17:44:36 +0530

    login process done

--- Backend/routes/UserRoutes.js
+++ Backend/routes/UserRoutes.js
@@ -12,5 +12,13 @@ router.post('/register',[
     userController.registerUser
 )
 
+router.post('/login',
+    [
+        body('email').isEmail().withMessage('Invalid Email'),
+        body('password').isLength({ min: 6 }).withMessage("Password length should be minimum of 6 characters")
+    ],
+    userController.loginUser
+)
+
 
 module.exports= router;
